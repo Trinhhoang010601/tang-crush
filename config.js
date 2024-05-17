d124d853d1c7231b8615ee24b45aec1f874fedb5
trinhhoang1601 <86735744+trinhhoang1601@users.noreply.github.com>
2024-05-17 16:30:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <3',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 8 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰 Yêu em rứt nhiều  😘😘',
     messDesc: 'Alo là có mặt luôn nè :>',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/trixuhoa'
 }
